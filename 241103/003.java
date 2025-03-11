b0ec4cdb4ef8fc96f5eb93be3a9d0c896dd7e6e2
gainedsomereason <1368444910@qq.com>
2025-03-11 09:10:15 +0800

    Add files via upload

---
+++ 241103/003.java
@@ -0,0 +1,80 @@
+package yszl;
+
+public class Ngzs {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+/**
+ * 1，
+ * web前端开发技术栈
+ * 	html css js
+ * 	异步交互技术Ajax,Axios
+ * 	前端js框架Vue,基于Vue的桌面端组件库Element，
+ * 	前端程序部署的服务器Nginx
+ * 	前端工程化，基于Vue的脚手架来构建Vue项目来开发
+ * web后端开发
+ * 	java项目的构建工具maven
+ * 	SpringBoot基础，web程序的开发，Spring的IOC,DI等，http协议，web服务器，tomcat
+ * 	SpringMVC基础
+ * 	MySQL,主流的关系型数据库，数据库的设计，数据库的操作，数据库的优化等，基于页面原型及需求文档，设计数据库表
+ * 	JDBC Mybatis，操作mysql
+ * 	SpringBoot整合SSM，根据页面原型、需求、接口文档编写接口
+ * 	会话跟踪技术，Cookie，Session，令牌技术(JMT)
+ * 	过滤器Filter，Spring MVC当中的拦截器Interceptor
+ * 	spring当中的AOP
+ * 	SpringBoot原理
+ * 
+ * 前后端分离开发，基于接口交互(接口文档)，基于页面原型和接口文档开发业务接口的能力
+ * 前端-基于Vue脚手架，构建工程化的前端项目
+ * 后端-基于主流SpringBoot高效学习SSM
+ * 通过案例贯穿整个课程体系，学以致用
+ * 参照企业开发模式，需求分析-表结构设计-接口文档-功能实现-测试
+ * 
+ * 接口文档管理平台YAPI：API管理平台
+ * 
+ * 2，
+ * web：全球广域网，万维网(www world wide web)能够通过浏览器访问的网站
+ * 网站工作流程
+ * 	浏览器，前端服务器(前端程序)，数据库服务器(数据库程序)，(后端服务器)java程序
+ * 前端代码转换成网页
+ * 	通过浏览器解析和渲染成用户看到的网页
+ * 	浏览器中对代码进行解析和渲染的部分，称为浏览器内核
+ * web标准，定义统一的浏览器内核
+ * 	网页标准，由一系列的标准组成，大部分由W3C(World Wide Web Consortium,万维网联盟)负责制定
+ * 	三个组成部分
+ * 		HTML:负责网页的结构(页面元素和内容)
+ * 		CSS:负责网页的表现(页面元素的外观、位置等页面样式，如:颜色、大小等)
+ * 		JavaScript:负责网页的行为(交互效果)
+ * 	基于JS封装的高级框架Vue，基于Vue封装的桌面端组件库Element
+ * 
+ * 3.
+ * HTML(HyperText Markup Language):超文本标记语言
+ * 	超文本：超越了文本的限制，比普通文本更强大，除了文字信息，还可以自定义图片、音频、视频等内容
+ * 	标记语言：由标签构成的语言
+ * 		HTML标签都是预定义好的，例如：使用<a>展示超链接，使用<img>展示图片，<video>展示视频
+ * 		HTML代码直接在浏览器中运行，HTML标签由浏览器解析
+ * xml可扩展的标记语言
+ * 	<user>
+ * 		<name>金毛狮王</name>
+ * 		<age>55</age>
+ * 		<address>
+ * 			<province>天京</province>
+ * 			<city>昌平</city>
+ * 		</address>
+ * 	</user>
+ * 
+ * CS(Cascading Style Sheet):层叠样式表，用于控制页面的样式(表现)
+ * 
+ * 官方文档w3school.com.cn
+ * 	https://www.w3school.com.cn/tags/index.asp
+ * 基础标签
+ * 	
+ * 
+ * 
+ * 
+ */
---
+++ 241103/005.java
@@ -0,0 +1,18 @@
+package yszl;
+
+public class Ngzs {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+/**
+ * maven操作
+ * 	clean移除上一次构建生成的文件
+ * 	compile编译项目源代码
+ * 	test使用合适的单元测试框架进行测试(junit)
+ * 	package将编译后的文件打包，如:jar、war等
+ * 	install安装项目到本地仓库
+ */
